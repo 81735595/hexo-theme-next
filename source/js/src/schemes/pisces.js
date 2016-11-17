6f9d40ff225dbc052201b8481a68e7504c4c881d
zhengxingcheng <xingcheng.zheng@gmail.com>
2016-11-17 09:03:06 +0800

    fixed nonstandard code

--- source/js/src/schemes/pisces.js
+++ source/js/src/schemes/pisces.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-  var $headerInner = $('.header-inner')
+  var $headerInner = $('.header-inner');
   var $sidebar = $('#sidebar');
   var getSidebarTop = function(){
-    return $headerInner.height() + 10
-  }
+    return $headerInner.height() + 10;
+  };
   var setSidebarMarginTop = function(sidebarTop){
-    return $sidebar.css({ 'margin-top': sidebarTop })
+    return $sidebar.css({ 'margin-top': sidebarTop });
   }
-  var mql = window.matchMedia("(min-width: 991px)")
+  var mql = window.matchMedia('(min-width: 991px)');
 
-  setSidebarMarginTop(getSidebarTop()).show()
+  setSidebarMarginTop(getSidebarTop()).show();
   mql.addListener(function(e){
     if(e.matches){
-      setSidebarMarginTop(getSidebarTop())
+      setSidebarMarginTop(getSidebarTop());
     }
-  })
+  });
 });
